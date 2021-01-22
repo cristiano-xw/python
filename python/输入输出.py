de77cc7a73cec2f68914d0ec9692b9d92bd87efd
cristiano-xw <1531887383@qq.com>
2021-01-22 20:54:04 +0800

    python add commit

---
+++ python/输入输出.py
@@ -0,0 +1,9 @@
+ranxia = input("请输入：")   #请输入：love ranxia
+print(ranxia)                
+ranxia = "love"
+xiaobin = "xiao"
+print(ranxia)      #love
+print(xiaobin)     #xiao
+print("-----------")
+print(ranxia,end=" ")
+print(xiaobin,end=" ") #love xiao
